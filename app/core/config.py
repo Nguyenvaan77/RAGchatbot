3b627d1315324553170d672f9c63a3e660bf27a2
Nguyenvaan77 <anvan16022005@gmail.com>
2026-05-31 08:48:54 +0700

    Hoàn thiện 70% chatbot

--- app/core/config.py
+++ app/core/config.py
@@ -2,6 +2,7 @@ import os
 
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(override=True)
 
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
--- scripts/ingest_csv.py
+++ scripts/ingest_csv.py
@@ -1,5 +1,12 @@
 from langchain_community.document_loaders import CSVLoader
 from langchain_core.documents import Document
+import sys
+import os
+
+# Add RAGchatbot directory to Python path to enable app imports
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from app.services.embedding_service import vector_store
 
 system_guide_faq_csv_path = "./data/failure-QA-data/failure-QA-data.csv"
 failure_guide_faq_csv_path = "./data/system-guide-data/system-guide-data.csv"
@@ -13,7 +20,7 @@ def load_from_csv_path_and_format_data():
     formatted_documents = []
 
     for path in csv_paths:
-        loader = CSVLoader(file_path=path,encoding="utf-8")
+        loader = CSVLoader(file_path=path, encoding="utf-8")
         raw_documents_per_path = loader.load()
 
         for doc in raw_documents_per_path:
@@ -23,18 +30,32 @@ def load_from_csv_path_and_format_data():
             answer = lines[1].replace("answer: ", "")
 
             formatted_content = f"""
-                                question: {question}
-                                answer: {answer}
-                                """.strip()
+question: {question}
+answer: {answer}
+""".strip()
 
             formatted_doc = Document(
                 page_content=formatted_content,
                 metadata={
                     "source": doc.metadata["source"],
                     "language": "vi"
-                    }
-                )
+                }
+            )
 
             formatted_documents.append(formatted_doc)
 
     return formatted_documents
+
+def run_ingestion():
+    print("Starting CSV data ingestion...")
+    docs = load_from_csv_path_and_format_data()
+    print(f"Loaded {len(docs)} documents. Ingesting into Chroma Vector DB...")
+    
+    # Thêm tài liệu vào vector store
+    vector_store.add_documents(docs)
+    print("Ingested documents successfully!")
+
+
+if __name__ == "__main__":
+    run_ingestion()
+
